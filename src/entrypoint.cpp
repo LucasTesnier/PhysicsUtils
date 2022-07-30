ae012c93b12d0c2da0e3950512df734fca2ac510
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-07-30 02:01:03 +0200

    [+] Add src cmake files Close #3

---
+++ src/entrypoint.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** PhysicsUtils
+** File description:
+** entrypoint
+*/
+
+/// \file src/entrypoint.cpp
+
+#include <iostream>
+
+void templated(void)
+{
+    std::cout << "Welcome to the physics library template function !"
+    << std::endl;
+}
---
+++ src/include/entrypoint.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** PhysicsUtils
+** File description:
+** entrypoint
+*/
+
+/// \file src/include/entrypoint.hpp
+
+#ifndef ENTRYPOINT_HPP_
+    #define ENTRYPOINT_HPP_
+
+/// \brief Just a basic template function for compilation setup
+void templated(void);
+
+#endif /* !ENTRYPOINT_HPP_ */
